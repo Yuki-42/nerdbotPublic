7b1b2a9c0d7aacd858ec751a77b3a5e0330129d1
Yuki <kylepannan@gmail.com>
2024-04-18 14:54:54 +1000

    Reworked config

--- internals/config.py
+++ internals/config.py
@@ -4,9 +4,13 @@ Contains the config for the project.
 # Standard Imports
 from json import dump, load
 from pathlib import Path
+from os import environ
+
+# Third Party Imports
+from dotenv import load_dotenv as loadDotEnv
 
 # Package Relative Imports
-from .logging_ import createLogger, SuppressedLoggerAdapter
+from .logging import createLogger, SuppressedLoggerAdapter
 from .errors import InvalidStatusType
 
 
@@ -15,23 +19,71 @@ class Config:
     The config class for the bot. This class is used to store the config values for the bot.
     """
     # Type Hints
-    configFilePath: Path
+    _configFilePath: Path
     logger: SuppressedLoggerAdapter
 
-    def __init__(self, configFile: Path = Path("BotData/config.json")) -> None:
+    dbIp: str
+    dbPort: str
+    dbName: str
+    dbUser: str
+    dbPassword: str
+    debug: bool = False
+    owonerId: str
+
+    def __init__(
+            self,
+            configJson: Path = Path("BotData/config.json"),
+            envFile: Path = Path("BotData/.env")
+    ) -> None:
         """
         Initialises the config class.
 
         Args:
-            configFile (Path): The path to the config file from the current working directory.
+            configJson (Path): The path to the config file.
+            envFile (Path): The path to the environment file.
 
         Returns:
             None
         """
-        self.configfile = configFile
+        self._configFilePath = configJson
 
         self.logger = createLogger("Config")
         self.logger.info("Initializing config")
+
+        # Load the environment variables
+        loadDotEnv(dotenv_path=envFile)
+
+        # Set the environment variables
+        self.dbIp = environ.get("DB_IP")
+        self.dbPort = environ.get("DB_PORT")
+        self.dbName = environ.get("DB_NAME")
+        self.dbUser = environ.get("DB_USER")
+        self.dbPassword = environ.get("DB_PASS")
+        self.debug = environ.get("DEBUG") == "True"
+        self.owonerId = environ.get("OWNER_ID")
+
+    """
+========================================================================================================================
+        Environment Variables
+========================================================================================================================
+    """
+    @property
+    def token(self) -> str:
+        """
+        Gets the bot token.
+
+        Returns:
+            str: The bot token.
+        """
+        if self.debug:
+            return environ.get("DEBUG_TOKEN")
+        return environ.get("TOKEN")
+
+    """
+========================================================================================================================
+        Json Config File
+========================================================================================================================
+    """
 
     @property
     def status(self) -> str:
@@ -41,7 +93,7 @@ class Config:
         Returns:
             str: The status.
         """
-        return self.getValue("status")
+        return self.getJson("status")
 
     @status.setter
     def status(self, value: str) -> None:
@@ -65,7 +117,7 @@ class Config:
         Returns:
             str: The status type.
         """
-        return self.getValue("statusType")
+        return self.getJson("statusType")
 
     @statusType.setter
     def statusType(self, value: str) -> None:
@@ -82,72 +134,23 @@ class Config:
             None
         """
         self.logger.debug(f"Attempting to set statusType to {value}")
-        match value.lower():
-            case "playing":
-                pass
-            case "watching":
-                pass
-            case "listening":
-                pass
-            case "streaming":
-                pass
-            case _:
-                raise InvalidStatusType(value)
+        if value not in ["playing", "watching", "listening", "streaming"]:
+            self.logger.error(f"Invalid status type '{value}'")
+            raise InvalidStatusType(value)
 
         self.setValue("statusType", value)
-
-    @property
-    def filterEnabled(self) -> bool:
-        """
-        Gets whether the filter is enabled or not.
-
-        Returns:
-            bool: Whether the filter is enabled or not.
-        """
-        return self.getValue("filterEnabled")
-
-    @filterEnabled.setter
-    def filterEnabled(self, value: bool) -> None:
-        """
-        Sets the filterEnabled value in the config file.
-
-        Args:
-            value (bool): The value to set filterEnabled to
-
-        Returns:
-            None
-        """
-        self.logger.debug(f"Setting filterEnabled to {value}")
-        self.setValue("filterEnabled", value)
-
-    @property
-    def owonerId(self) -> list[int]:
-        """
-        Gets the owner id.
-        Returns:
-            int: The owner id.
-        """
-        return self.getValue("owonerId")
 
     @property
     def gifClassifiers(self) -> list[str]:
         """
         Gets the gif classifiers.
+
         Returns:
             list: The gif classifiers.
         """
-        return self.getValue("gifClassifiers")
+        return self.getJson("gifClassifiers")
 
-    @property
-    def loggingLevel(self) -> str:
-        """
-        Gets the logging level.
-        Returns:
-            str: The logging level.
-        """
-        return self.getValue("loggingLevel")
-
-    def getValue(self, key) -> str | int | bool | list[str | int]:
+    def getJson(self, key) -> str | int | bool | list[str | int]:
         """
         Gets a value from the config file.
         Args:
@@ -156,7 +159,7 @@ class Config:
         Returns:
             The value of the key.
         """
-        with open(self.configfile, 'r') as file:
+        with open(self._configFilePath, 'r') as file:
             configData: dict[str, str | int | bool | list[str]] = load(file)
 
         try:
@@ -173,10 +176,10 @@ class Config:
             value: The value to write.
         """
         self.logger.debug(f"Writing new value for key '{key}': {value}")
-        with open(self.configfile, 'r') as file:
+        with open(self._configFilePath, 'r') as file:
             configData: dict[str, str | int | bool | list[str]] = load(file)
 
         configData[key] = value
 
-        with open(self.configfile, 'w') as file:
+        with open(self._configFilePath, 'w') as file:
             dump(configData, file, indent=4)
